5288e38f17d06377410bad6b63d2897b218d2d86
keyshafaradina <keyshakey050@gmail.com>
2026-04-28 21:23:43 +0700

    nambah frontend react

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'API jalan'
+    ]);
+});
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
